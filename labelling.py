50b48dce8b9c4c674f40e9104cb7715e3be4cb81
whatwant <whatwant@gmail.com>
2021-06-21 23:27:41 +0900

    불량 블로거 일괄 처리 기능 추가

--- labelling.py
+++ labelling.py
@@ -13,19 +13,35 @@ if __name__ == '__main__':
     db = firestore.client()
 
     collection = db.collection(u'dongtan_raw')
-    items = collection.where(u'result', u'==', u'NA').stream()
+    items = list(collection.where(u'result', u'==', u'NA').order_by(u'bloggername').get())
 
-
+    items_count = len(items)
+    print(f"Total : {items_count}")
+    grouping = ""
     for idx, content in enumerate(items):
 
         item = content.to_dict()
+
+        print(f"[{idx+1}/{items_count}] ", end='')
+
+        if( grouping != "" ):
+            if( item['bloggername'] == grouping ):
+                item['result'] = False
+                doc = item['link'].split("=")[-1]
+                collection.document(doc).update({u'result': item['result']})
+                print("pass")
+                print("")
+                continue
+            else:
+                grouping = ""
+
 
         print(f"bloggername: {item['bloggername']}")
         print(f"title: {item['title']}")
         print(f"postdate: {item['postdate']}")
         print(f"description: {item['description']}")
         print("")
-        print("1.Private, 2.Commercial")
+        print("1.Private, 2.Commercial, 3.Commercial(bloggername)")
 
         label = input()
 
@@ -34,6 +50,10 @@ if __name__ == '__main__':
 
         elif( label == "2" ):
             item['result'] = False
+
+        elif( label == "3" ):
+            item['result'] = False
+            grouping = item['bloggername']
 
         else:
             continue
